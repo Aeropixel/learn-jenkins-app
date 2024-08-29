1af8e15b500b0d1fb27026b02578621b4c5e11fd
António Cabral <antonio.ml.cabral@gmail.com>
2024-08-29 21:36:00 +0000

    Making tests to fail on purpose

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn Jenkins link', () => {
-  render(<App />);
+  render(<Appz />);
   const linkElement = screen.getByText(/learn Jenkins/i);
   expect(linkElement).toBeInTheDocument();
 });
